2c69355813b54999558d0c475b1602a68145e46c
Erica Liljeblad <erica.liljeblad@levelnine.se>
2022-08-02 11:51:42 +0200

    Add missing files

---
+++ components/markdown.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Markdown from 'markdown-to-jsx';
+
+const StyledMarkdown = styled.div`
+  a {
+    color: rgb(13, 28, 70);
+    transition: color 0.2s ease;
+
+    &:hover {
+      color: rgb(45, 74, 147);
+    }
+  }
+`;
+
+const MarkdownComp = ({ children, ...props }) => {
+  return (
+    <StyledMarkdown>
+      <Markdown>
+        {children}
+      </Markdown>
+    </StyledMarkdown>
+  );
+};
+
+export default MarkdownComp;
+
+
+
+
+
